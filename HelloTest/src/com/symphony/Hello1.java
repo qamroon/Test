deab514c468b86c6e3c1c413e0cd86587f9e77e5
qamroon <alam.btech@gmail.com>
2012-06-28 17:28:35 +0530

    One line added in Hello1.java  in HelloTest project under Test Repository on Remote (Git hub).

--- HelloTest/src/com/symphony/Hello1.java
+++ HelloTest/src/com/symphony/Hello1.java
@@ -15,6 +15,7 @@ public class Hello1 {
 	public static void main(String[] args) {
 	System.out.println("Hi");
 	System.out.println("Hi modifying the file");
+         System.out.println("Hi this line on Remote ");
 	
 
 	}
