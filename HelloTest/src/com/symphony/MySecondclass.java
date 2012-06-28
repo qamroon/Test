40b1b37aaf1c698b38f82d53eab7e63d56b3f0fb
qamroon <alam.btech@gmail.com>
2012-06-28 17:19:40 +0530

    Adding MySecondclass.

---
+++ HelloTest/src/com/symphony/MySecondclass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.symphony;
+
+/**
+ * @author nalam
+ *
+ */
+public class MySecondclass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hi adding the second hello class");
+
+	}
+
+}
