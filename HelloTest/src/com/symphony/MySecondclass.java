7182bfcccc615826aad9b9b3fba15527260c4411
qamroon <alam.btech@gmail.com>
2012-06-28 21:10:21 +0530

    commit for Second helloclass at 9:10

--- HelloTest/src/com/symphony/MySecondclass.java
+++ HelloTest/src/com/symphony/MySecondclass.java
@@ -14,7 +14,7 @@ public class MySecondclass {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hi adding the second hello class");
-
+		System.out.println("Hi adding the second line in the Second class");
 	}
 
 }
