349b0e6302afadec64d41133430bb2de8f1e7314
qamroon <alam.btech@gmail.com>
2012-06-28 21:16:36 +0530

    commit.

--- HelloTest/src/com/symphony/MySecondclass.java
+++ HelloTest/src/com/symphony/MySecondclass.java
@@ -13,7 +13,7 @@ public class MySecondclass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hi adding the second hello class");
+	 	System.out.println("Hi adding the second hello class");
 		System.out.println("Hi adding the second line in the Second class");
 	}
 
