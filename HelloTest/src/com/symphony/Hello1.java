63715ad88a13c68933727d740e69d8f1dcccee10
qamroon <alam.btech@gmail.com>
2012-06-28 20:09:22 +0530

    commiting the third line on 28 /06/ at 8.09

--- HelloTest/src/com/symphony/Hello1.java
+++ HelloTest/src/com/symphony/Hello1.java
@@ -15,7 +15,7 @@ public class Hello1 {
 	public static void main(String[] args) {
 	System.out.println("Hi");
 	System.out.println("Hi modifying the file");
-	
+	System.out.println("Hi adding the third line the file");
 
 	}
 
