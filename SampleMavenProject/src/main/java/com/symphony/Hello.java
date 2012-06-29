07cb2df61c2e6316f5cacf894282ced523c0c718
qamroon <alam.btech@gmail.com>
2012-06-29 11:06:46 +0530

    hi Sample MavenProject.

---
+++ SampleMavenProject/src/main/java/com/symphony/Hello.java
@@ -0,0 +1,13 @@
+package com.symphony;
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome in SampleMavenProject");
+
+	}
+
+}
