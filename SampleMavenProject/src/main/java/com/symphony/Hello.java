c21c922e546f59b69d6f7c2d8a55a7188f6e6bb3
ZHAOAML <mohalam4@in.ibm.com>
2021-01-07 18:39:18 +0530

    latest change

--- SampleMavenProject/src/main/java/com/symphony/Hello.java
+++ SampleMavenProject/src/main/java/com/symphony/Hello.java
@@ -7,6 +7,9 @@ public class Hello {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome in SampleMavenProject");
+		System.out.println("Welcome in SampleMavenProject from Local");
+		System.out.println("some code writen");
+		
 
 	}
 
